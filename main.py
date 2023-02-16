5474cc9cb8ef36b673ebd415113246b63016d779
Nariman <Nariman079i>
2023-02-16 21:01:51 +0300

    Вывод темперетуры кипеня

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 from time import sleep
-
+from keyboard import *
 
 def liter_limit(liter:float,a:float,b:float):
+    try:
+        if a < liter <= b:
+            return 1
 
-    if a < liter <= b:
-        return 1
+        elif liter < 0 :
+            return 2
 
-    elif liter<0 :
-        return 2
-
-    elif liter==0:
-        return 3
-
-    else:
-        return 0
+        elif liter==0:
+            return 3
+        else:
+            return 0
+    except ValueError:
+        return 4
 
 class Teapot:
     
@@ -24,18 +25,22 @@ class Teapot:
 
     def run(self):
         print()
-        print("Сколько литров воды воды вы хотите налить в чайник")
+        print("Сколько литров воды вы хотите налить в чайник")
 
         self.liter = float(input("( 0 - 1.0 ) :"))
 
         action = liter_limit(self.liter, 0, 1.0)
 
         if action == 1 :
-            time = int(self.liter * 5)
+            time = int(self.liter * self.time_boiling)
             print("Чайник вкл")
-            sleep(time)
-            print("Чайник начал кипеть")
-            sleep(time)
+            t = 0
+            while t <= 100:
+                print(f"{int(t)}°")
+                t += self.time_boiling * 2
+                sleep((1.0 - (1.0-self.liter)))
+    
+            
             print("Чайник вскипел")
             print("Чайник выкл")
         elif action == 2:
@@ -46,10 +51,13 @@ class Teapot:
             print("Упс, чайник пустой")
             print("Попробуйте ещё раз")
             return self.run()
+        elif action == 4:
+            print("Нужны литры, а не символы")
+            print("Попробуйте ещё раз")
+            return self.run()
         else:
             print("Чайник не вмещает в себя болше одного литра")
             print("Попробуйте ещё раз")
             return self.run()
-
 
 teapot1 = Teapot()
