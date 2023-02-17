0daedf59170d92b4f4640e6f4e4d56e9481281d3
Nariman <Nariman079i>
2023-02-17 18:16:15 +0300

    Создания файла конфигурации

--- main.py
+++ main.py
@@ -1,57 +1,82 @@
 import sys
 import logging
+import configs
 from time import sleep
 from keyboard import *
 
 
-def liter_limit(liter:float, a: float, b: float):
+# "Валидация" для чайника
+def liter_limit(liter:float, a: float, b=configs.VOLUME):
     try:
         liter = float(liter)
         if a < liter <= b:
+            """
+            
+            b - объем
+            """
             return 1
 
         elif liter < 0:
+            """
+            литры не могут быть отрицательными
+            """
             return 2
 
         elif liter == 0:
+            """
+            чайник включенный без воды, ни к чему хорошему не приведет
+            """
             return 3
-        else:
+        else:#
             return 0
-    except:
+    except:#
+        """
+        тут срабатывает исключение если человек введет символ
+        """
         return 4
 
 
 class Teapot:
-    def __init__(self, liter: int = None, time_boiling=10):
+    def __init__(self, liter: int = None, time_boiling=configs.TIME_BOILING):
         self.liter = liter
         self.time_boiling = time_boiling / 2  # Время закипания
 
     def run(self):
-        logging.basicConfig(level=logging.DEBUG, filename="log.log", format='%(asctime)s %(levelname)s:%(message)s')
+        """
+        Главная функция для работы с чайником
+        """
+        logging.basicConfig(level=logging.DEBUG, filename="log.log", format='%(asctime)s %(levelname)s:%(message)s')#
         print()
         print("Сколько литров воды вы хотите налить в чайник")
         print("Если захотите остановить чайник удерживайте 'A' eng")
-        self.liter = input("( 0 - 1.0 ) :")
+        self.liter = input("( 0 - 1.0 ) :")#
 
-        action = liter_limit(self.liter, 0, 1.0)
+        action = liter_limit(self.liter, 0)# Проверка налитой воды -- "Валидация" :)
 
         if action == 1:
 
             print("Чайник вкл")
             logging.debug('Start Teapot')
             t = 0
-            while True:
+            while True:# Цикл для отображения температуры
                 try:
-                    if is_pressed("a"):
+                    if is_pressed("a"):#
                         sys.exit()
 
                     print(f"{int(t)}°")
-                    if t == 100.0:
+                    if t == configs.TEMP:#Температура кипения
                         break
-                    t += self.time_boiling * float(self.liter)
+                    t += self.time_boiling  #
 
-                    sleep((1.0 - (1.0 - float(self.liter))))
+                    sleep(abs(1.0 - (1.0 - float(self.liter))))
+                    """
+                    Скорость кипения зависит от налитой воды в чайник
+                    чем меньше воды, тем быстрее он вскипит 
+                    """
                 except:
+                    """
+                    исключение если нужно будет остановить кипение чайника
+                    """
                     logging.error('Stop Teapot')
                     print("Чайник остановлен")
                     quit()
@@ -83,7 +108,6 @@ class Teapot:
             return self.run()
 
 
-teapot1 = Teapot()
+teapot1 = Teapot() #Класс A
 
-
-teapot1.run()
+teapot1.run() #Экземпляр класса А
