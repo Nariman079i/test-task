002ca13f35f50dfee710a7ca69ac19a424802907
Nariman <Nariman079i>
2023-02-17 17:13:35 +0300

    Логирование файла

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from time import sleep
 from keyboard import *
 
 
-def liter_limit(liter: float, a: float, b: float):
+def liter_limit(liter:float, a: float, b: float):
     try:
+        liter = float(liter)
         if a < liter <= b:
             return 1
 
@@ -16,7 +17,7 @@ def liter_limit(liter: float, a: float, b: float):
             return 3
         else:
             return 0
-    except ValueError:
+    except:
         return 4
 
 
@@ -26,54 +27,63 @@ class Teapot:
         self.time_boiling = time_boiling / 2  # Время закипания
 
     def run(self):
+        logging.basicConfig(level=logging.DEBUG, filename="log.log", format='%(asctime)s %(levelname)s:%(message)s')
         print()
         print("Сколько литров воды вы хотите налить в чайник")
         print("Если захотите остановить чайник удерживайте 'A' eng")
-        self.liter = float(input("( 0 - 1.0 ) :"))
+        self.liter = input("( 0 - 1.0 ) :")
 
         action = liter_limit(self.liter, 0, 1.0)
 
         if action == 1:
-            time = int(self.liter * self.time_boiling)
+
             print("Чайник вкл")
+            logging.debug('Start Teapot')
             t = 0
             while True:
                 try:
-
                     if is_pressed("a"):
                         sys.exit()
 
                     print(f"{int(t)}°")
                     if t == 100.0:
                         break
-                    t += self.time_boiling * 2
+                    t += self.time_boiling * float(self.liter)
 
-                    sleep((1.0 - (1.0 - self.liter)))
+                    sleep((1.0 - (1.0 - float(self.liter))))
                 except:
+                    logging.error('Stop Teapot')
                     print("Чайник остановлен")
                     quit()
 
+            logging.debug('End Teapot')
             print("Чайник вскипел")
+            logging.debug('Off Teapot')
             print("Чайник выкл")
         elif action == 2:
-            print("В чайнкие не может быть отрицательное значение литров")
+            logging.error('Error code  2')
+            print("В чайнике не может быть отрицательное значение литров")
             print("Попробуйте ещё раз")
             return self.run()
         elif action == 3:
+            logging.error('Error code  3')
             print("Упс, чайник пустой")
             print("Попробуйте ещё раз")
             return self.run()
         elif action == 4:
+            logging.error('Error code  4')
             print("Нужны литры, а не символы")
             print("Попробуйте ещё раз")
+
             return self.run()
         else:
-            print("Чайник не вмещает в себя болше одного литра")
+            logging.error('Error code  0')
+            print("Чайник не вмещает в себя больше одного литра")
             print("Попробуйте ещё раз")
             return self.run()
 
 
 teapot1 = Teapot()
 
-if __name__ == '__main__':
-    teapot1.run()
+
+teapot1.run()
